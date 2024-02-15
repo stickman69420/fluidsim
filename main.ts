89afd95e570fb7904fbb1e0510f4eeb934053065
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2024-02-15 15:59:13 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,23 +6,6 @@ namespace SpriteKind {
     export const pin = SpriteKind.create()
     export const box = SpriteKind.create()
 }
-/**
- * Pick some fruit and peel it.
- */
-//% weight=70 icon="\uf185" color=#EC7505
-namespace Fluids {
-    /**
-     * Pick a fruit
-     */
-    //% blockId=tropic_pick block="pick a %fruit"
-    export function CreateDrop (myImage: Image) {
-        let mySprite3 = sprites.create(myImage, SpriteKind.Liquid)
-    }
-}
-let mySprite2: Sprite = null
-let mySprite: Sprite = null
-let i = 0
-let i2 = false
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite2.setFlag(SpriteFlag.Invisible, true)
 })
@@ -59,6 +42,19 @@ sprites.onCreated(SpriteKind.Liquid, function (sprite) {
         mySprite.setFlag(SpriteFlag.Invisible, true)
     }
 })
+let i2 = false
+let i = 0
+let mySprite: Sprite = null
+let mySprite2: Sprite = null
+namespace Fluids {
+    /**
+     * Pick a fruit
+     */
+    //% blockId=tropic_pick block="Liquids"
+    export function CreateDrop (myImage: Image) {
+        let mySprite3 = sprites.create(myImage, SpriteKind.Liquid)
+    }
+}
 forever(function () {
     i = 0
     for (let value of sprites.allOfKind(SpriteKind.Liquid)) {
@@ -67,25 +63,25 @@ forever(function () {
         sprites.allOfKind(SpriteKind.hidbogsrite)[i].setPosition(value.x + 5, value.y)
         i += 1
     }
-    for (let value of sprites.allOfKind(SpriteKind.hidbox)) {
+    for (let value2 of sprites.allOfKind(SpriteKind.hidbox)) {
         i2 = false
-        for (let value2 of sprites.allOfKind(SpriteKind.pin)) {
-            if (value.overlapsWith(value2)) {
+        for (let value22 of sprites.allOfKind(SpriteKind.pin)) {
+            if (value2.overlapsWith(value22)) {
                 i2 = true
             }
         }
-        for (let value2 of sprites.allOfKind(SpriteKind.box)) {
-            if (value.overlapsWith(value2)) {
+        for (let value23 of sprites.allOfKind(SpriteKind.box)) {
+            if (value2.overlapsWith(value23)) {
                 i2 = true
             }
         }
-        for (let value2 of sprites.allOfKind(SpriteKind.Liquid)) {
-            if (value.overlapsWith(value2)) {
+        for (let value24 of sprites.allOfKind(SpriteKind.Liquid)) {
+            if (value2.overlapsWith(value24)) {
                 i2 = true
             }
         }
         if (i2) {
-            value.setImage(img`
+            value2.setImage(img`
                 f f f f f 
                 . . . . . 
                 . . . . . 
@@ -93,7 +89,7 @@ forever(function () {
                 . . . . . 
                 `)
         } else {
-            value.setImage(img`
+            value2.setImage(img`
                 1 1 1 1 1 
                 . . . . . 
                 . . . . . 
@@ -102,25 +98,25 @@ forever(function () {
                 `)
         }
     }
-    for (let value of sprites.allOfKind(SpriteKind.hidboxleft)) {
+    for (let value3 of sprites.allOfKind(SpriteKind.hidboxleft)) {
         i2 = false
-        for (let value2 of sprites.allOfKind(SpriteKind.pin)) {
-            if (value.overlapsWith(value2)) {
+        for (let value25 of sprites.allOfKind(SpriteKind.pin)) {
+            if (value3.overlapsWith(value25)) {
                 i2 = true
             }
         }
-        for (let value2 of sprites.allOfKind(SpriteKind.box)) {
-            if (value.overlapsWith(value2)) {
+        for (let value26 of sprites.allOfKind(SpriteKind.box)) {
+            if (value3.overlapsWith(value26)) {
                 i2 = true
             }
         }
-        for (let value2 of sprites.allOfKind(SpriteKind.Liquid)) {
-            if (value.overlapsWith(value2)) {
+        for (let value27 of sprites.allOfKind(SpriteKind.Liquid)) {
+            if (value3.overlapsWith(value27)) {
                 i2 = true
             }
         }
         if (i2) {
-            value.setImage(img`
+            value3.setImage(img`
                 . . . . f 
                 . . . . f 
                 . . . . f 
@@ -128,7 +124,7 @@ forever(function () {
                 . . . . f 
                 `)
         } else {
-            value.setImage(img`
+            value3.setImage(img`
                 . . . . 1 
                 . . . . 1 
                 . . . . 1 
@@ -137,25 +133,25 @@ forever(function () {
                 `)
         }
     }
-    for (let value of sprites.allOfKind(SpriteKind.hidbogsrite)) {
+    for (let value4 of sprites.allOfKind(SpriteKind.hidbogsrite)) {
         i2 = false
-        for (let value2 of sprites.allOfKind(SpriteKind.pin)) {
-            if (value.overlapsWith(value2)) {
+        for (let value28 of sprites.allOfKind(SpriteKind.pin)) {
+            if (value4.overlapsWith(value28)) {
                 i2 = true
             }
         }
-        for (let value2 of sprites.allOfKind(SpriteKind.box)) {
-            if (value.overlapsWith(value2)) {
+        for (let value29 of sprites.allOfKind(SpriteKind.box)) {
+            if (value4.overlapsWith(value29)) {
                 i2 = true
             }
         }
-        for (let value2 of sprites.allOfKind(SpriteKind.Liquid)) {
-            if (value.overlapsWith(value2)) {
+        for (let value210 of sprites.allOfKind(SpriteKind.Liquid)) {
+            if (value4.overlapsWith(value210)) {
                 i2 = true
             }
         }
         if (i2) {
-            value.setImage(img`
+            value4.setImage(img`
                 f . . . . 
                 f . . . . 
                 f . . . . 
@@ -163,7 +159,7 @@ forever(function () {
                 f . . . . 
                 `)
         } else {
-            value.setImage(img`
+            value4.setImage(img`
                 1 . . . . 
                 1 . . . . 
                 1 . . . . 
@@ -173,7 +169,7 @@ forever(function () {
         }
     }
     i = 0
-    for (let value of sprites.allOfKind(SpriteKind.Liquid)) {
+    for (let value5 of sprites.allOfKind(SpriteKind.Liquid)) {
         if (sprites.allOfKind(SpriteKind.hidbox)[i].image.equals(img`
             1 1 1 1 1 
             . . . . . 
@@ -181,7 +177,7 @@ forever(function () {
             . . . . . 
             . . . . . 
             `)) {
-            value.y += 1
+            value5.y += 1
         }
         if (Math.percentChance(50)) {
             if (sprites.allOfKind(SpriteKind.hidboxleft)[i].image.equals(img`
@@ -191,7 +187,7 @@ forever(function () {
                 . . . . 1 
                 . . . . 1 
                 `)) {
-                value.x += -1
+                value5.x += -1
             }
         } else {
             if (sprites.allOfKind(SpriteKind.hidbogsrite)[i].image.equals(img`
@@ -201,7 +197,7 @@ forever(function () {
                 1 . . . . 
                 1 . . . . 
                 `)) {
-                value.x += 1
+                value5.x += 1
             }
         }
         i += 1
